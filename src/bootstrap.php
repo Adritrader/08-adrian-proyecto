194c12a54c3d262302b53d7d62ba9d573cd73491
Adritrader <adri_denia_123@hotmail.com>
2021-01-13 12:31:29 +0100

    first commit

---
+++ src/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\App;
+use App\Core\Response;
+use App\Database;
+use App\Utils\MyLogger;
+use App\Utils\MyMail;
+use App\Core\Helpers\FlashMessage;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+$config = require_once __DIR__ . '/../config/config.php';
+
+
+App::bind("config", $config);
+$_flash = new FlashMessage();
+App::bind("flash", $_flash);
+App::bind("DB", Database::getConnection());
+App::bind(Response::class, new Response());
+
+$myLogger = new MyLogger("app");
+$myLogger->pushHandler(new StreamHandler(__DIR__."/../{$config["logfile"]}", $config["loglevel"]));
+App::bind(MyLogger::class, $myLogger);
+
+// The load method acts as a factory. We pass the config
+// data and returns a myMail object
+$myMail = MyMail::load($config["mailer"]);
+App::bind(MyMail::class, $myMail);
